51b7da98a5739bdb8938f43feddc8d27a285a960
Mateus Riff <mateusnriff@gmail.com>
2023-08-02 15:55:30 -0300

    feat(styles): Implement styles for the project
    Squash and merge the following commits into develop:
    * feat(styles): add GlobalStyle
    * feat(styles): add styles.ts
    * feat(styles): import more font weights from Manrope

---
+++ src/styles/styles.ts
@@ -0,0 +1,30 @@
+const styles = {
+    colors: {
+        primary: '#469C57',
+        secondary: '#1A3C21',
+        gray: '#303030',
+        lightGray: '#D9D9D9',
+        darkGray: '#606060',
+        orange: '#E8833A1',
+        white: '#F9F9F9',
+    },
+    fontSizes: {
+        xs: '1.125rem',
+        sm: '1.25rem',
+        md: '1.375rem',
+        lg: '1.5rem',
+        xl: '1.875rem',
+    },
+    fontWeights: {
+        thin: '200',
+        light: '300',
+        regular: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+        extrabold: '800',
+    }
+};
+  
+export default styles;
+  
