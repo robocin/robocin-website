171b0ddb346b43e0cbd490f02f314cc50ab5d360
mateusriff <mateusnriff@gmail.com>
2023-08-02 15:35:46 -0300

    feat(styles): import more font weights from Manrope

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from 'react';
 import { Manrope } from 'next/font/google'
 
-const manrope = Manrope({ subsets: ['latin'] })
+const manrope = Manrope({ subsets: ['latin'], weight: ['200', '300', '400', '500', '600', '700', '800'] })
 
 interface LayoutProps {
     children: ReactNode;
