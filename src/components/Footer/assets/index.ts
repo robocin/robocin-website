57df4f5797d9082308fe5115067bc6a701e74fc7
mateusriff <mateusnriff@gmail.com>
2023-08-14 18:23:23 -0300

    feat(Footer): add new assets

---
+++ src/components/Footer/assets/index.ts
@@ -0,0 +1,7 @@
+import Facebook from './facebook.svg'
+import Github from './github.svg'
+import Instagram from './instagram.svg'
+import LinkedIn from './linkedIn.svg'
+import Youtube from './youtube.svg'
+
+export { Facebook, Github, Instagram, LinkedIn, Youtube }
