aa7ca8e1c6f2f375bf5a228549258e83bfbcdd25
mateusriff <mateusnriff@gmail.com>
2023-08-19 18:39:12 -0300

    update(tests): update Footer tests

--- __tests__/components/Footer/index.test.tsx
+++ __tests__/components/Footer/index.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import { render } from '@testing-library/react'
+import { useRouter } from 'next/router'
 import { Footer } from '@/components'
 import type * as ReactDom from 'react-dom'
 
@@ -7,6 +8,26 @@ import type * as ReactDom from 'react-dom'
 jest.mock('react-dom', () => ({
   ...jest.requireActual<typeof ReactDom>('react-dom'),
   preload: jest.fn(),
+}))
+
+// Mocking next/router to provide useRouter functionality
+jest.mock('next/router', () => ({
+  useRouter: () => ({
+    locale: 'en',
+    pathname: '/',
+    push: jest.fn(),
+  }),
+}))
+
+// Mock the useTranslation hook
+jest.mock('../../../src/hooks/useTranslation', () => () => ({
+  footer: {
+    location: {
+      label: 'Find Us',
+      address:
+        'Ave. Jorn. Aníbal Fernandes, no number - Cidade Universitária, Recife, PE, Brazil. Zip Code: 50740-560',
+    },
+  },
 }))
 
 // Test 1: Render the Footer without any errors
