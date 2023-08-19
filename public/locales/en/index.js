91828e98fee05a7ea1f6cd737600c25131524910
mateusriff <mateusnriff@gmail.com>
2023-08-19 18:30:55 -0300

    update(languages): update content for footer

--- public/locales/en/index.js
+++ public/locales/en/index.js
@@ -1,13 +1,21 @@
 const en = {
-    navbar: {
-        options: {
-            home: 'Home',
-            categories: 'Categories',
-            team: 'Team',
-            papers: 'Papers'
-        },
+  navbar: {
+    options: {
+      home: 'Home',
+      categories: 'Categories',
+      team: 'Team',
+      papers: 'Papers',
     },
-    content: 'This content is in English'
+  },
+  footer: {
+    contact: 'Contact Us',
+    location: {
+      label: 'Find Us',
+      address:
+        'Ave. Jorn. Aníbal Fernandes, no number - Cidade Universitária, Recife, PE, Brazil. Zip Code: 50740-560',
+    },
+  },
+  content: 'This content is in English',
 }
 
-export default en;
+export default en
