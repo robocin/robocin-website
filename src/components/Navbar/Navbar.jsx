a9c055298ad2770b512c9a82838c9e25c0aded62
mateusriff <mateusnriff@gmail.com>
2026-03-05 21:06:31 -0300

    feat: add home option to navigation bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -29,6 +29,7 @@ function Navbar() {
         </button>
 
         <ul className={`${styles.links} ${menuOpen ? styles.linksOpen : ''}`}>
+          <li><NavLink to="/" end onClick={close}>{t('nav.home')}</NavLink></li>
           <li><NavLink to="/categories" onClick={close}>{t('nav.categories')}</NavLink></li>
           <li><NavLink to="/blog" onClick={close}>{t('nav.blog')}</NavLink></li>
           <li><NavLink to="/publications" onClick={close}>{t('nav.publications')}</NavLink></li>
